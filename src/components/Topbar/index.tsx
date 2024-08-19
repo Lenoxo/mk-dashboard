f73287aafe5a97955f580745371fba1ea1e5d800
Emanuel Padilla <emanuehl159@gmail.com>
2024-08-18 19:34:12 -0500

    Add basic style with dummy data to TopBar component

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -2,10 +2,24 @@ import "./styles.css";
 
 export function TopBar() {
   return (
-    <nav>
-      <p>Player 1</p>
-      <p className="vs-text">VS</p>
-      <p>Rival 1</p>
+    <nav className="topbar">
+      <div className="player-thumbnail">
+        <p className="player-thumbnail__nickname">Player 1</p>
+        <img
+          className="player-thumbnail__picture"
+          src="https://avatarfiles.alphacoders.com/359/thumb-1920-359966.jpg"
+        />
+      </div>
+      <span className="topbar__result-counter-1">2</span>
+      <p className="topbar__vs-text">VS</p>
+      <span className="topbar__result-counter-2">1</span>
+      <div className="player-thumbnail">
+        <p className="player-thumbnail__nickname">Player 2</p>
+        <img
+          className="player-thumbnail__picture"
+          src="https://avatarfiles.alphacoders.com/362/thumb-1920-362804.jpg"
+        />
+      </div>
     </nav>
   );
 }
