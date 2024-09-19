c5527564dde2672b6d8e4cfe83b74721274656eb
Emanuel Padilla <emanuehl159@gmail.com>
2024-09-18 20:18:24 -0500

    Add basic states; profileData, currentDayFights and charactersData

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
+import { useEffect, useState } from "react";
 import { AddFightButton } from "./components/AddFight";
 import { FightResult } from "./components/FightResult";
 import { TopBar } from "./components/Topbar";
 import "./styles.css";
 
+const now = new Date();
+const currentDate = now.toLocaleDateString();
+// const formatDate = { year: "numeric", month: "numeric", day: "numeric" };
+
 function App() {
+  const [profileData, setProfileData] = useState({
+    nickname: "Lenoxo",
+    image: "https://avatarfiles.alphacoders.com/359/thumb-1920-359966.jpg",
+    rivals: [
+      {
+        nickname: "Rival 1",
+        image: "https://avatarfiles.alphacoders.com/362/thumb-1920-362804.jpg",
+      },
+    ],
+    history: [
+      {
+        date: currentDate,
+        rival: "Rival 1",
+        character1: "Scorpion",
+        character2: "Subzero",
+        win: true,
+      },
+    ],
+  });
+
+  const [charactersData, setCharactersData] = useState([
+    { name: "Scorpion", imageUrl: "/scorpion.png" },
+    { name: "Subzero", imageUrl: "/subzero.png" },
+  ]);
+
+  const [currentDayFights, setCurrentDayFights] = useState([]);
+
+  useEffect(() => {
+    if (profileData.history.length > 0) {
+      setCurrentDayFights(profileData.history);
+      // setTimeout(() => {
+      //   console.table(currentDayFights);
+      // }, 1000);
+      console.table(profileData.history);
+    }
+  }, []);
+
+  console.info(profileData);
   return (
     <>
       {/* <header>MK-Dashboard</header> */}
-      <TopBar />
+      <TopBar
+        playerName={profileData.nickname}
+        playerImage={profileData.image}
+        rivalName={profileData.rivals[0].nickname}
+        rivalImage={profileData.rivals[0].image}
+      />
       <main>
         <FightResult />
         <FightResult />
--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,24 +1,18 @@
 import "./styles.css";
 
-export function TopBar() {
+export function TopBar({ playerName, playerImage, rivalName, rivalImage }) {
   return (
     <nav className="topbar">
       <div className="player-thumbnail">
-        <p className="player-thumbnail__nickname">Player 1</p>
-        <img
-          className="player-thumbnail__picture"
-          src="https://avatarfiles.alphacoders.com/359/thumb-1920-359966.jpg"
-        />
+        <p className="player-thumbnail__nickname">{playerName}</p>
+        <img className="player-thumbnail__picture" src={playerImage} />
       </div>
       <span className="topbar__result-counter-1">2</span>
       <p className="topbar__vs-text">VS</p>
       <span className="topbar__result-counter-2">1</span>
       <div className="player-thumbnail">
-        <p className="player-thumbnail__nickname">Player 2</p>
-        <img
-          className="player-thumbnail__picture"
-          src="https://avatarfiles.alphacoders.com/362/thumb-1920-362804.jpg"
-        />
+        <p className="player-thumbnail__nickname">{rivalName}</p>
+        <img className="player-thumbnail__picture" src={rivalImage} />
       </div>
     </nav>
   );
