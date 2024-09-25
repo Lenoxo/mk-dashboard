0fe0b4a4d185704cb9decc6e2cca2fccbed47c80
Emanuel Padilla <emanuehl159@gmail.com>
2024-09-24 23:44:30 -0500

    Add basic page for /profile and AsideNav component for quick navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+  useRoutes,
+} from "react-router-dom";
+import { ProfilePage } from "./pages/profile.tsx";
+import { AsideNav } from "./components/AsideNav/index.tsx";
+
+function AppRoutes() {
+  const routes = useRoutes([
+    {
+      // TODO: Añadir una página de error personalizada con la propiedad de errorPage.
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/profile",
+      element: <ProfilePage />,
+    },
+  ]);
+  return routes;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AsideNav />
+      <AppRoutes />
+    </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/pages/profile.tsx
@@ -0,0 +1,3 @@
+export function ProfilePage() {
+  return <div>Hello world from /profile</div>;
+}
