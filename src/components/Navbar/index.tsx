c67dbd2518d923a6c7580c9f1791411863422445
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-08 18:27:48 -0500

    Add icons and basic style to navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+import "./styles.css";
+
+export function AsideNav() {
+  return (
+    <nav className="navbar">
+      <NavLink className="navbar__link" to="/">
+        <svg
+          className="navbar__link__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        <p className="navbar__link__text">Home</p>
+      </NavLink>
+      <NavLink className="navbar__link" to="/profile">
+        <svg
+          className="navbar__link__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+          />
+        </svg>
+        <p className="navbar__link__text">Profile</p>
+      </NavLink>
+      <NavLink className="navbar__link" to="/history">
+        <svg
+          className="navbar__link__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        <p className="navbar__link__text">History</p>
+      </NavLink>
+    </nav>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AsideNav } from "./components/AsideNav/index.tsx";
+import { AsideNav } from "./components/Navbar/index.tsx";
 import { AppProvider } from "./context/index.tsx";
 import { AppRoutes } from "./pages/AppRoutes.tsx";
 
