221d298da5d9363c1fbd209b75ca7dcdd3a1fd67
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-09 13:31:38 -0500

    Add good enough sidebar button and styles from mobile view
    Also, removed unused console.info

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,20 +1,49 @@
 import { ReactNode, useState } from "react";
 import { Navbar } from "../Navbar";
+import "./styles.css";
 
 export function MainLayout({ children }: { children: ReactNode }) {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-  console.info(isMenuOpen);
   return (
     <>
-      <header>
-        Hello there, this is the header.
-        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          Click to toggle
-        </button>
-      </header>
       <Navbar open={isMenuOpen} setOpen={setIsMenuOpen} />
-      {children}
+      <div className={`appWrapper ${isMenuOpen && "appWrapper--move"}`}>
+        <Header open={isMenuOpen} setOpen={setIsMenuOpen} />
+        {children}
+      </div>
     </>
+  );
+}
+
+interface Props {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+}
+
+function Header({ open, setOpen }: Props) {
+  return (
+    <header className="header">
+      Mk-Dashboard
+      <button
+        className={`header__button ${open && "header__button--disabled"}`}
+        disabled={open ? true : false}
+        onClick={() => setOpen(!open)}
+      >
+        <svg
+          className="header__button__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+    </header>
   );
 }
