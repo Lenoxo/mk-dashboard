71e471368fde639d65790e8b3063553b51f52040
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-17 20:33:35 -0500

    Add delete button in RivalQuickInfo and WIP delete

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,15 +6,53 @@ import { Modal } from "../../components/Modal";
 import { RivalForm } from "../../components/RivalForm";
 import { RivalQuickInfo } from "../../components/RivalQuickInfo";
 import "./styles.css";
+import { Rival } from "../../types";
 
 export function ProfilePage() {
   const context = useContext(AppContext);
   if (!context) {
     throw new Error("AppContext should be used inside an AppProvider");
   }
-  const { profileData } = context;
+  const { profileData, historyEntries, setProfileData, setHistoryEntries } =
+    context;
 
   const [openModal, setOpenModal] = useState<boolean>(false);
+
+  function handleRivalDelete(rivalId: Rival["id"]) {
+    const updatedHistoryEntries = { ...historyEntries };
+
+    for (
+      let index = 0;
+      index < Object.values(updatedHistoryEntries).length;
+      index++
+    ) {
+      let dayFightsArray = Object.values(updatedHistoryEntries)[index];
+      let date = "";
+      const filteredFights = dayFightsArray.filter((entry) => {
+        date = entry.date;
+        return entry.rivalId !== rivalId;
+      });
+      
+      console.warn(filteredFights)
+
+      dayFightsArray = filteredFights;
+
+      if (dayFightsArray.length === 0) {
+        delete updatedHistoryEntries[date];
+      }
+    }
+
+    setHistoryEntries(updatedHistoryEntries);
+
+    // const updatedProfileData = { ...profileData };
+    // const rivals = updatedProfileData.rivals;
+    // const rivalIndex = rivals.findIndex((rival) => rival.id === rivalId);
+
+    // rivals.splice(rivalIndex, 1);
+
+    // setProfileData(updatedProfileData);
+  }
+  
   return (
     <>
       <QuickInfo />
@@ -32,6 +70,7 @@ export function ProfilePage() {
               rivalId={rival.id}
               name={rival.nickname}
               image={rival.image}
+              deleteRival={handleRivalDelete}
             />
           );
         })}
