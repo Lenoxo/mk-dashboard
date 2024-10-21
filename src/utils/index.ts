2430fa074c34ab0176ab7a7c34d472b68b94bc15
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-21 14:25:27 -0500

    Add helper for asyncLocalStorage and working reading of profileData and
    HistoryEntries from localStorage values in /context/index.tsx

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,20 @@
 const now = new Date();
 const currentDate = now.toLocaleDateString();
 
-export { currentDate };
+const asyncLocalStorage = {
+  setItem(key: string, value: object): Promise<string> {
+    return new Promise((resolve) => {
+      localStorage.setItem(key, JSON.stringify(value));
+      resolve("Data saved");
+    });
+  },
+
+  getItem(key: string): Promise<string | null> {
+    return new Promise((resolve) => {
+      const item = localStorage.getItem(key);
+      resolve(item);
+    });
+  },
+};
+
+export { currentDate, asyncLocalStorage };
