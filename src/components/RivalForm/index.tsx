e5ac7528df322be40e192601917f771bde23d8c7
Emanuel Padilla <emanuehl159@gmail.com>
2024-10-22 12:18:37 -0500

    Fix profileData as null build error in RivalForm

--- src/components/RivalForm/index.tsx
+++ src/components/RivalForm/index.tsx
@@ -26,32 +26,37 @@ export function RivalForm({ setOpenModal }: Props) {
 
   const { id } = useParams();
 
+  if (!profileData) {
+    throw new Error(
+      "profileData is null, you should not render this component when this happens",
+    );
+  }
+
   useEffect(() => {
     if (!id) {
       // Because in this case, the user is using the form in /profile
       // No in /profile/rivals/:id
       return;
-    } else {
-      if (typeof id !== "string") {
-        throw new Error("The current rival id in the url does not exist");
-      }
+    }
+    if (typeof id !== "string") {
+      throw new Error("The current rival id in the url does not exist");
+    }
 
-      let tempIndex = 0;
+    let tempIndex = 0;
 
-      for (let i = 0; i < profileData.rivals.length; i++) {
-        if (profileData.rivals[i].id !== id) {
-          continue;
-        }
-        tempIndex = i;
-        setRivalIndex(i);
-        break;
+    for (let i = 0; i < profileData.rivals.length; i++) {
+      if (profileData.rivals[i].id !== id) {
+        continue;
       }
+      tempIndex = i;
+      setRivalIndex(i);
+      break;
+    }
 
-      if (profileData.rivals[tempIndex]) {
-        setRivalData(profileData.rivals[tempIndex]);
-        setImageValue(profileData.rivals[tempIndex].image);
-        return;
-      }
+    if (profileData.rivals[tempIndex]) {
+      setRivalData(profileData.rivals[tempIndex]);
+      setImageValue(profileData.rivals[tempIndex].image);
+      return;
     }
   }, [id, profileData.rivals]);
 
@@ -74,6 +79,12 @@ export function RivalForm({ setOpenModal }: Props) {
   }
 
   function addNewRival(newRivalData: Rival) {
+    if (!profileData) {
+      throw new Error(
+        "profileData is null, you should not render this component when this happens",
+      );
+    }
+
     const updatedRivals = [...profileData.rivals];
     updatedRivals.push(newRivalData);
 
@@ -88,6 +99,12 @@ export function RivalForm({ setOpenModal }: Props) {
   }
 
   function updateRivalData(updatedRivalData: Rival) {
+    if (!profileData) {
+      throw new Error(
+        "profileData is null, you should not render this component when this happens",
+      );
+    }
+
     const updatedRivals = [...profileData.rivals];
     updatedRivals[rivalIndex] = updatedRivalData;
 
