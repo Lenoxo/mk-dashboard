2f1cf5682b861e2abd187a89bb58d094aca9dbe2
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-07 21:48:33 -0500

    Add rest of the tests for fizzbuzz using TDD

---
+++ src/utils/fizzbuzz.js
@@ -0,0 +1,8 @@
+export function fizzbuzz(num) {
+  if (typeof num !== "number") throw new Error("parameter is not a number");
+
+  if ((num % 3 === 0) & (num % 5 === 0)) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
+  return num;
+}
--- tests/firstSteps.test.js
+++ tests/firstSteps.test.js
@@ -1,4 +1,5 @@
 import { describe, expect, test } from "vitest";
+import { fizzbuzz } from "../src/utils/fizzbuzz";
 
 /*
 Debe haber una función que reciba un número de parametro y:
@@ -9,16 +10,38 @@ Debe haber una función que reciba un número de parametro y:
 4. Retorne el número si ninguno de los casos anteriores se cumple
 */
 
-function fizzbuzz(num) {
-  if (typeof num !== "number") throw new Error();
-}
-
 describe("fizzbuzz", () => {
-  test("fizzbuzz should be a function", () => {
-    expect(typeof fizzbuzz).toBe("function");
+  // Este test ya está cubierto por los siguientes
+  // test("fizzbuzz should be a function", () => {
+  //   expect(typeof fizzbuzz).toBe("function");
+  // });
+
+  // Este test ya está cubierto por los siguientes
+  // test("fizzbuzz should throw an error if parameter is not a number", () => {
+  //   expect(() => fizzbuzz()).toThrow();
+  // });
+
+  test("fizzbuzz should throw specific error if parameter is not a number", () => {
+    expect(() => fizzbuzz()).toThrow("parameter is not a number");
   });
 
-  test("fizzbuzz should throw an error if parameter is not a number", () => {
-    expect(() => fizzbuzz()).toThrow();
+  test("fizzbuzz should return a number if a valid parameter is received", () => {
+    expect(fizzbuzz(2)).toBe(2);
+  });
+
+  test("fizzbuzz should return fizz if parameter is a multiple of 3", () => {
+    expect(fizzbuzz(3)).toBe("fizz");
+    expect(fizzbuzz(6)).toBe("fizz");
+    expect(fizzbuzz(12)).toBe("fizz");
+  });
+
+  test("fizzbuzz should return buzz if parameter is a multiple of 5", () => {
+    expect(fizzbuzz(5)).toBe("buzz");
+    expect(fizzbuzz(25)).toBe("buzz");
+  });
+
+  test("fizzbuzz should return fizzbuzz if parameter is a multiple of 3 and 5", () => {
+    expect(fizzbuzz(60)).toBe("fizzbuzz");
+    expect(fizzbuzz(15)).toBe("fizzbuzz");
   });
 });
