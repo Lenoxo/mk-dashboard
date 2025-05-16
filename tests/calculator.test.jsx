d2da2ed7570e01caa099f04aadb3f45c8b9916ba
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-15 20:23:57 -0500

    Fix prettier config not being detected by vscode

--- tests/calculator.test.jsx
+++ tests/calculator.test.jsx
@@ -1,54 +1,52 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, expect, test } from "vitest";
 
-const rows = [
-    [ 7, 8, 9 ], 
-    [ 4, 5, 6 ],
-    [ 1, 2, 3 ],
-    [ 0 ]
-]
+const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
 
 const Calculator = () => {
-    return (
-        <section>
-            <h1>Calculator</h1>
-        <div role="grid">
+  return (
+    <section>
+      <h1>Calculator</h1>
+      <div role="grid">
         {rows.map((row, index) => {
-        return <div key={index} role="row">
-            {row.map(num => {
-                return <span key={num}>{num}</span>
-            })}
-        </div>
+          return (
+            <div key={index} role="row">
+              {" "}
+              {row.map((num) => {
+                return <span key={num}>{num}</span>;
+              })}
+            </div>
+          );
         })}
-        </div>
-        </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-describe('Calculator', () => {
-    afterEach(cleanup)
-    test('should render', () => {
-        render(<Calculator />)
-    })
-    
-    test('should show title', () => {
-        render(<Calculator />)
-        
-        screen.getByText('Calculator')
-    })
-    
-    
-    test('should show numbers', () => {
-        render(<Calculator />)
-        const numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+describe("Calculator", () => {
+  afterEach(cleanup);
+  test("should render", () => {
+    render(<Calculator />);
+  });
 
-        
-        numbers.forEach(number => { screen.getByText(number) }) })
-    
-    test('should have 4 rows for numbers', () => {
-        render(<Calculator />)
-        
-        expect(screen.getAllByRole('row').length).toBe(4)
-    })
-    
-})
+  test("should show title", () => {
+    render(<Calculator />);
+
+    screen.getByText("Calculator");
+  });
+
+  test("should show numbers", () => {
+    render(<Calculator />);
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    numbers.forEach((number) => {
+      screen.getByText(number);
+    });
+  });
+
+  test("should have 4 rows for numbers", () => {
+    render(<Calculator />);
+
+    expect(screen.getAllByRole("row").length).toBe(4);
+  });
+});
