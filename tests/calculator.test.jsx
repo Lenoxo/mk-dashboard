71ee4cc83a6d3d51eb44f0da4a3a6f9fa41ca1e3
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-17 21:24:20 -0500

    Add operations render and showing the value in an input

--- tests/calculator.test.jsx
+++ tests/calculator.test.jsx
@@ -1,21 +1,41 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { useState } from "react";
 import { afterEach, describe, expect, test } from "vitest";
 
 const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
 
+const operations = ["+", "-", "*", "/"];
+
 const Calculator = () => {
+  const [value, setValue] = useState("");
+
+  const handleButtonPress = (newValue) => setValue(value.concat(newValue));
+
   return (
     <section>
       <h1>Calculator</h1>
+      <input value={value} readOnly />
       <div role="grid">
         {rows.map((row, index) => {
           return (
             <div key={index} role="row">
-              {" "}
               {row.map((num) => {
-                return <span key={num}>{num}</span>;
+                return (
+                  <button onClick={() => handleButtonPress(num)} key={num}>
+                    {num}
+                  </button>
+                );
               })}
             </div>
+          );
+        })}
+      </div>
+      <div role="column">
+        {operations.map((op) => {
+          return (
+            <button onClick={() => handleButtonPress(op)} key={op}>
+              {op}
+            </button>
           );
         })}
       </div>
@@ -44,9 +64,57 @@ describe("Calculator", () => {
     });
   });
 
-  test("should have 4 rows for numbers", () => {
+  test("should show 4 rows for numbers", () => {
     render(<Calculator />);
 
     expect(screen.getAllByRole("row").length).toBe(4);
+  });
+
+  test("should show an input element", () => {
+    render(<Calculator />);
+
+    screen.getByRole("textbox");
+  });
+
+  test("should show the value in the input element when pressing 1 button", () => {
+    render(<Calculator />);
+
+    const button1 = screen.getByText("1");
+    fireEvent.click(button1);
+
+    const input = screen.getByRole("textbox");
+    expect(input.value).toBe("1");
+  });
+
+  test("should show the value in the input element when pressing several buttons", () => {
+    render(<Calculator />);
+
+    const button1 = screen.getByText("1");
+    fireEvent.click(button1);
+    const button2 = screen.getByText("2");
+    fireEvent.click(button2);
+    const button3 = screen.getByText("3");
+    fireEvent.click(button3);
+    const button4 = screen.getByText("4");
+    fireEvent.click(button4);
+
+    const input = screen.getByRole("textbox");
+    expect(input.value).toBe("1234");
+  });
+
+  test("should show the value in the input element when pressing several buttons and the operation", () => {
+    render(<Calculator />);
+
+    const button4 = screen.getByText("4");
+    fireEvent.click(button4);
+
+    const addition = screen.getByText("+");
+    fireEvent.click(addition);
+
+    const button2 = screen.getByText("2");
+    fireEvent.click(button2);
+
+    const input = screen.getByRole("textbox");
+    expect(input.value).toBe("4+2");
   });
 });
